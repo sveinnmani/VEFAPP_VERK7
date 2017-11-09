aa91da586bf078f00b514e6e8e06bc4c12e1ee06
Jóhann Rúnarsson <johann9898@gmail.com>
2017-11-09 22:50:19 +0000

    hmm
    er en að reyna að gera multi array

--- application/steamauth/gameOwnApi.php
+++ application/steamauth/gameOwnApi.php
@@ -1,36 +1,55 @@
 <?php 
- 
+
 if (isset($_SESSION["steamid"])) {
+        error_reporting(0);
         $url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=AFF824E1547B93172F0918DE382825BF&steamid='. $steamprofile['steamid'] .'&format=json'; //replace "CLIENT-ID"
         $json = file_get_contents($url);
         $gameowned = json_decode($json, true);
         $totalgame = $gameowned['response']['game_count'];
-        for ($i=0; $i < $totalgame; $i++) { 
+        for ($i=0; $i < $totalgame; $i++) {
             $appid[] = $gameowned['response']['games'][$i]['appid'];
             $playtime[] = $gameowned['response']['games'][$i]['playtime_forever'];
         }
+        $url_list = array();
+        for ($i=0; $i < $totalgame; $i++) {
 
-       
-        $ach_url = 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=' . $appid[2] . '&key=AFF824E1547B93172F0918DE382825BF&steamid=' . $steamprofile['steamid'];
-        	$ach_json = file_get_contents($ach_url);
-        	$achievGame = json_decode($ach_json, true);
+            array_push('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=' . $appid[$i] . '&key=AFF824E1547B93172F0918DE382825BF&steamid=' . $steamprofile['steamid']);
+            $ach_json[] = file_get_contents($ach_url[$i]);
+
+            if($ach_json === false){
+            echo 'Nothing here!';   
+            } else {
+                $achievGame[] = json_decode($ach_json[$i], true);
+                for ($i=0; $i < $totalgame; $i++) {
+                
+                    $achievName = $achievGame['playerstats']['achievements'][0]['name'];
+                
+                }
+                print_r(count($achievGame[1]['playerstats']['achievements']));
+            }
+        }
         
-        if(!empty($achievGame)){
-  			echo 'Nothing here!';   
-		} else {
-  			echo 'Yep, found it!';
-		}
+        
+        
+  			    
+
+
+            /*$achieve_url = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/   v2/?key=AFF824E1547B93172F0918DE382825BF&appid='. $appid[3];
+            $achieve_json = file_get_contents($achieve_url);
+            $achieveArray = json_decode($achieve_json, true);
+            $achieveName = $achieveArray['game']['achievements'][0]['name'];*/
+		
         
 
         
 
       
-        $achievName = $achievGame['playerstats']['achievements'][0]['name'];
+        /*$achievName = $achievGame['playerstats']['achievements'][0]['name'];
 
 
         $achieve_url = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=AFF824E1547B93172F0918DE382825BF&appid='. $appid[3];
         $achieve_json = file_get_contents($achieve_url);
-        $achieveArray = json_decode($achieve_json, true);
+        $achieveArray = json_decode($achieve_json, true);*/
             
 
         /*for ($i=0; $i < $totalgame; $i++) { 
