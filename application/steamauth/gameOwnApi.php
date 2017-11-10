853f6a71d09aa5d6a2bd96e8c8fb905c24536155
Jóhann Rúnarsson <johann9898@gmail.com>
2017-11-10 10:18:16 +0000

    sasdas
    asdasda

--- application/steamauth/gameOwnApi.php
+++ application/steamauth/gameOwnApi.php
@@ -1,7 +1,7 @@
 <?php 
 
 if (isset($_SESSION["steamid"])) {
-        error_reporting(0);
+        //error_reporting(0);
         $url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=AFF824E1547B93172F0918DE382825BF&steamid='. $steamprofile['steamid'] .'&format=json'; //replace "CLIENT-ID"
         $json = file_get_contents($url);
         $gameowned = json_decode($json, true);
@@ -10,24 +10,52 @@ if (isset($_SESSION["steamid"])) {
             $appid[] = $gameowned['response']['games'][$i]['appid'];
             $playtime[] = $gameowned['response']['games'][$i]['playtime_forever'];
         }
-        $url_list = array();
-        for ($i=0; $i < $totalgame; $i++) {
 
-            array_push('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=' . $appid[$i] . '&key=AFF824E1547B93172F0918DE382825BF&steamid=' . $steamprofile['steamid']);
+        $ch = curl_init();
+        for ($i=0; $i < $totalgame; $i++) {
+            curl_setopt($ch, CURLOPT_URL, 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=' . urlencode($appid[$i]) . '&key=AFF824E1547B93172F0918DE382825BF&steamid=' . urlencode($steamprofile['steamid']));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $achieve[] = curl_exec($ch);
+   
+            if(curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200) {
+        
+                $achievGame[] = json_decode($achieve[$i]);
+                if($achievGame[$i]->status == 'OK'){
+                    for ($v=0; $v < $totalgame; $v++) {
+                        for ($a=0; $a < count($achievGame[$v]['playerstats']['achievements']); $a++) { 
+
+                            $achievName[] = $achievGame[$v]['playerstats']['achievements'][$a]['name'];
+                        }
+                    }
+                }
+                else{
+                    return $achievName[] = 'dot';
+                }
+            }
+        }
+        curl_close($ch);
+       
+        /*for ($i=0; $i < $totalgame; $i++) {
+
+            $ach_url[] ='http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=' . $appid[$i] . '&key=AFF824E1547B93172F0918DE382825BF&steamid=' . $steamprofile['steamid'];
             $ach_json[] = file_get_contents($ach_url[$i]);
 
             if($ach_json === false){
             echo 'Nothing here!';   
             } else {
+                
                 $achievGame[] = json_decode($ach_json[$i], true);
+
+                
                 for ($i=0; $i < $totalgame; $i++) {
-                
-                    $achievName = $achievGame['playerstats']['achievements'][0]['name'];
-                
+                    for ($a=0; $a < count($achievGame[$i]['playerstats']['achievements']); $a++) { 
+
+                        $achievName[] = $achievGame[$i]['playerstats']['achievements'][$a]['name'];
+                    }
                 }
-                print_r(count($achievGame[1]['playerstats']['achievements']));
+               
             }
-        }
+        }*/
         
         
         
--- application/view/home/index.php
+++ application/view/home/index.php
@@ -2,4 +2,7 @@
     <h1>Home</h1>
     <p>application/view/home/index.php</p>
     <img src="<?php echo URL;?>img/demo-image.png">
+    <?php  
+    include dirname(__FILE__).'../../../steamauth/newsApi.php'
+    ?>
 </div>
