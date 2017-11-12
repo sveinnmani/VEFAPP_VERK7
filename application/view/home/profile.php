4feb67e0d78483c952e5a4e2c7fc93aba563a10d
sveinnmani <sveinnmani@hotmail.com>
2017-11-12 19:49:27 +0000

    Nota þetta profile
    Raða leikjum með semantic-ui

--- application/view/home/profile.php
+++ application/view/home/profile.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php	
 	if (!isset($_SESSION["steamid"])) {
 		header("location: http://174.138.67.190/SessionMini3Demo/");
 	}
@@ -7,20 +6,32 @@
 	{
 		include dirname(__FILE__).'../../../steamauth/gameOwnApi.php';
 	}
-	
-	
 ?>
-<div class="container">
-	<img src="">
-	<?php  
-		for ($i=0; $i < count($gameName); $i++) { 
-			echo '<div class="gameowned"> 
-					<img src="' . $bannerName[$i] . '">
-					<p>Game Owned: ' . $gameName[$i] . '</p>
-					<p>Total Played: ' . $playtime[$i] . '</p>
-					<br>
-					</div>';
-		}
-	?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="semantic.css">
+	</head>
+	<body>
+		<div class="ui grid">
+			<h1 class="ui header">Your game library:</h1>
+			<div class="three column row">
+				<?php  
+					for ($i=0; $i < count($gameName); $i++) {
+						echo '
+							<div class="column">
+								<div class="ui segment">
+									<img src="' . $bannerName[$i] . '">
+									<h4>Game name: ' . $gameName[$i] . '</h4>
+									<h5>Total hours Played: ' . round($playtime[$i]/60) . '</h5>
+								</div>
+							</div>';
 
-</div>
+
+					}
+				?>
+			</div>
+		</div>
+	</body>
+</html>
